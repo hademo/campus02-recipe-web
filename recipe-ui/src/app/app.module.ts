290f8f68cb2bc01cc75808fd071ce80b49d3239d
Hade Mohamed <hade.mohamed@outlook.de>
2023-01-14 11:42:07 +0100

    Recipe-Form, Recipe-List Skeleton

--- recipe-ui/src/app/app.module.ts
+++ recipe-ui/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RecipeFormComponent } from './recipes/recipe-form/recipe-form.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RecipeFormComponent,
+    RecipeListComponent,
   ],
   imports: [
     BrowserModule,
