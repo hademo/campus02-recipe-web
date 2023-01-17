efb429cb0aef87422aa6a6152b82aeeb43ea2a00
Raphael Pansi <raphael.pansi@datenkraft.com>
2023-01-17 21:47:37 +0100

    Resolve further merge conflicts

--- recipe-ui/src/app/services/recipe/recipe.service.ts
+++ recipe-ui/src/app/services/recipe/recipe.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Recipe } from '../../../lib/recipe.dto';
 import { StorageService } from '../storage/storage.service';
+import { jsPDF } from 'jspdf';
 
 @Injectable({
   providedIn: 'root',
@@ -37,5 +38,25 @@ export class RecipeService {
     return this.http.delete<Recipe>(
       `http://localhost:3000/recipes/${recipe.id}`
     );
+  }
+
+  public toPdf(recipe: Recipe) {
+    var doc = new jsPDF();
+    doc.setFont('helvetica');
+    doc.setFontSize(12);
+    doc.text(
+      doc.splitTextToSize(
+        `
+      Titel: ${recipe.name}\n
+      Beschreibung: ${recipe.description}\n
+      Zutaten: ${recipe.ingredients.join(', ')}
+      `,
+        180
+      ),
+      10,
+      20
+    );
+
+    window.open(URL.createObjectURL(doc.output('blob')));
   }
 }
