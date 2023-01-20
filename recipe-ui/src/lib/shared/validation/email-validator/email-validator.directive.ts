2d07e8aa89353b38b2fdc60705ef64138aa0d188
Raphael Pansi <raphael.pansi@datenkraft.com>
2023-01-20 20:55:28 +0100

    Add Validators, Child Component and Search Filter

---
+++ recipe-ui/src/lib/shared/validation/email-validator/email-validator.directive.ts
@@ -0,0 +1,42 @@
+import { Directive } from '@angular/core';
+import {
+  AbstractControl,
+  NG_VALIDATORS,
+  ValidationErrors,
+  Validator,
+} from '@angular/forms';
+
+@Directive({
+  selector: '[email]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: EmailValidatorDirective,
+      multi: true,
+    },
+  ],
+})
+export class EmailValidatorDirective implements Validator {
+  constructor() {}
+
+  validate(c: AbstractControl): ValidationErrors | null {
+    if (c.value && !this.isValidEmail(c.value)) {
+      return {
+        email: {
+          invalidEmail: c.value,
+        },
+      };
+    }
+
+    return null;
+  }
+
+  public isValidEmail = (email: string): boolean => {
+    var validRegex =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (email.match(validRegex)) {
+      return true;
+    }
+    return false;
+  };
+}
